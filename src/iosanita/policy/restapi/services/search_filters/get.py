0e69cde63e77375fa8de288ff03f88d9725788ee
Andrea Cecchi <andrea.cecchi85@gmail.com>
2024-08-18 15:05:41 +0200

    add new field and @search-filters endpoint

---
+++ src/iosanita/policy/restapi/services/search_filters/get.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+from iosanita.policy import _
+from plone import api
+from plone.restapi.interfaces import ISerializeToJsonSummary
+from plone.restapi.services import Service
+from zope.component import getMultiAdapter
+from iosanita.policy.interfaces import IIoSanitaSettings
+from AccessControl.unauthorized import Unauthorized
+
+import json
+
+
+class SearchFiltersGET(Service):
+    def reply(self):
+        return {
+            "sections": self.get_section_data(field_id="search_sections"),
+            "quick_search_sections": self.get_section_data(
+                field_id="quick_search_sections"
+            ),
+        }
+
+    def get_section_data(self, field_id):
+        try:
+            values = api.portal.get_registry_record(
+                field_id, interface=IIoSanitaSettings, default="[]"
+            )
+        except KeyError:
+            return []
+        utils = api.portal.get_tool(name="plone_utils")
+
+        sections = []
+        for setting in json.loads(values or "[]"):
+            items = []
+            for section_settings in setting.get("items") or []:
+                for uid in section_settings.get("linkUrl") or []:
+                    try:
+                        section = api.content.get(UID=uid)
+                    except Unauthorized:
+                        # private folder
+                        continue
+                    if not section:
+                        continue
+                    item_infos = getMultiAdapter(
+                        (section, self.request),
+                        ISerializeToJsonSummary,
+                    )()
+                    children = section.listFolderContents(
+                        contentFilter={"portal_type": utils.getUserFriendlyTypes()}
+                    )
+                    item_infos["items"] = [
+                        getMultiAdapter(
+                            (x, self.request),
+                            ISerializeToJsonSummary,
+                        )()
+                        for x in children
+                    ]
+                    item_infos["title"] = section_settings.get("title", "")
+                    items.append(item_infos)
+            if items:
+                sections.append(
+                    {
+                        "rootPath": setting.get("rootPath", ""),
+                        "items": items,
+                    }
+                )
+        return sections
