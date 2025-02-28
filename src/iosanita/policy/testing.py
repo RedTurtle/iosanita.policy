93e03b498b16253509d43d278263853face48b6d
Andrea Cecchi <andrea.cecchi85@gmail.com>
2025-02-28 17:39:01 +0100

    fix testing deps

--- src/iosanita/policy/testing.py
+++ src/iosanita/policy/testing.py
@@ -6,32 +6,42 @@ from plone.app.testing import IntegrationTesting
 from plone.testing.zope import WSGI_SERVER_FIXTURE
 from zope.configuration import xmlconfig
 
+import auslfe.farmacie
 import collective.feedback
 import collective.volto.dropdownmenu
 import collective.volto.enhancedlinks
 import collective.volto.formsupport
+import collective.volto.secondarymenu
 import collective.volto.slimheader
 import collective.volto.socialsettings
 import collective.volto.subfooter
+import collective.volto.subsites
 import iosanita.contenttypes
 import iosanita.policy
 import plone.app.caching
+import redturtle.faq
+import redturtle.voltoplugin.editablefooter
 import souper.plone
 
 
 class TestLayer(ContentTypesTestLayer):
     def setUpZope(self, app, configurationContext):
         super().setUpZope(app, configurationContext)
-        self.loadZCML(package=plone.app.caching)
-        self.loadZCML(package=iosanita.contenttypes)
-        self.loadZCML(package=collective.volto.enhancedlinks)
+        self.loadZCML(package=auslfe.farmacie)
         self.loadZCML(package=collective.feedback)
-        self.loadZCML(package=souper.plone)
-        self.loadZCML(package=collective.volto.slimheader)
         self.loadZCML(package=collective.volto.dropdownmenu)
+        self.loadZCML(package=collective.volto.enhancedlinks)
+        self.loadZCML(package=collective.volto.formsupport)
+        self.loadZCML(package=collective.volto.secondarymenu)
+        self.loadZCML(package=collective.volto.slimheader)
         self.loadZCML(package=collective.volto.socialsettings)
         self.loadZCML(package=collective.volto.subfooter)
-        self.loadZCML(package=collective.volto.formsupport)
+        self.loadZCML(package=collective.volto.subsites)
+        self.loadZCML(package=iosanita.contenttypes)
+        self.loadZCML(package=plone.app.caching)
+        self.loadZCML(package=redturtle.faq)
+        self.loadZCML(package=redturtle.voltoplugin.editablefooter)
+        self.loadZCML(package=souper.plone)
         self.loadZCML(package=iosanita.policy, context=configurationContext)
 
         xmlconfig.file(
